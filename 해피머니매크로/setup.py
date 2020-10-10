381ba6f15a4d31833f6e90ce92c48d0bbbececf2
SEONGJIN SON <thstjdwls10@naver.com>
2020-10-10 19:49:21 +0900

    설치파일

--- 해피머니매크로/setup.py
+++ 해피머니매크로/setup.py
@@ -1,33 +1,25 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Oct 10 19:37:16 2020
+import io
+from setuptools import find_packages, setup
 
-@author: seongjin.son
-"""
 
-from setuptools import setup, find_packages
+# Read in the README for the long description on PyPI
+def long_description():
+    with io.open('README.rst', 'r', encoding='utf-8') as f:
+        readme = f.read()
+    return readme
 
-with open('requirements.txt') as f:
-    requirements = f.read().splitlines()
-
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-
-setuptools.setup(
-    name="happy macro", # 이 패키지를 설치/삭제할 때 사용할 이름을 의미한다. 이 이름과 import할 때 쓰이는 이름은 다르다.
-    version="0.0.1",
-    author="seongjin",
-    author_email="seongjin@naver.com",
-    description="happy money macro ver.1",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/thstjdwls10/seongjin",
-    packages=setuptools.find_packages(),
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-    python_requires='>=3.6',
-    install_requires=requirements,
-)
+setup(name='algorithms',
+      version='0.1',
+      description='practice python with solving algorithms',
+      long_description=long_description(),
+      url='https://github.com/thstjdwls10/seongjin',
+      author='seongjin',
+      author_email='thstjdwls10@naver.com',
+      license='MIT',
+      packages=find_packages(),
+      classifiers=[
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.6',
+          ],
+      zip_safe=False)
